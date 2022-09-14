e947ff03b958c47452a97c86c5c85fefd346232e
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-09-14 14:26:04 +0700

    feat: create navbar component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-full h-1/5 bg-blue-500 navbar">
-    <h3 class="text-white font-bold">TO DO LIST APP</h3>
+  <div class="w-full h-1/5 py-8 bg-blue-500 navbar">
+    <h3 class="ml-32 text-2xl text-white navbar__brand">TO DO LIST APP</h3>
   </div>
 </template>
 
@@ -14,5 +14,9 @@ export default {
 .navbar {
   background-color: #16abf8;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+
+  &__brand {
+    font-weight: 700;
+  }
 }
 </style>
