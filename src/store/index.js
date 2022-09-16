26fefc945c85739a11a273fe9f7922d7d06dce0a
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-09-16 07:39:43 +0700

    feat: create state, getters, and mutation example

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,19 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    activityData: [],
   },
   getters: {
+    activityData(state) {
+      return state.activityData;
+    },
   },
   mutations: {
+    ADD_ACTIVITY_DATA(state, { data }) {
+      state.activityData = data;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
