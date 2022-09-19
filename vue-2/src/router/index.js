d5af55386ff37645afe1211921898185f458ef07
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2022-09-19 10:55:33 +0700

    feat: add activity detail page on vue 2 project

--- vue-2/src/router/index.js
+++ vue-2/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ActivityDetailView from "../views/ActivityDetailView.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/detail/:id",
+    name: "activity",
+    component: ActivityDetailView,
   },
 ];
 
---
+++ vue-2/src/views/ActivityDetailView.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="activity-detail">
+    <div class="mt-10 mx-56">
+      <div data-cy="todo-header" class="flex justify-between">
+        <div class="flex gap-4">
+          <span
+            data-cy="todo-back-button"
+            class="icon-back"
+            @click="$router.push('/')"
+          ></span>
+          <h1
+            v-if="!showInputText"
+            data-cy="todo-title"
+            class="text-4xl font-bold"
+          >
+            {{
+              activityDetailData.title?.length > 20
+                ? `${activityDetailData.title.slice(0, 20)}...`
+                : activityDetailData.title
+            }}
+          </h1>
+          <input
+            v-else-if="showInputText"
+            v-model="inputTextValue"
+            class="h-4/5 w-4/5 bg-inherit border-b-2 border-gray-700 text-3xl font-bold focus:outline-none"
+            type="text"
+            name="activityTitle"
+          />
+          <div
+            data-cy="todo-title-edit-button"
+            @click="changeActivityTitle"
+            class="mt-2 icon-edit-title"
+          ></div>
+        </div>
+        <div class="flex gap-4">
+          <button class="px-3 border border-gray-300 rounded-full">
+            <span class="icon-sort"></span>
+          </button>
+          <button
+            data-cy="todo-add-button"
+            class="px-8 py-3 rounded-3xl text-white text-lg font-medium add-button"
+            @click="toggleInputDialog"
+          >
+            <span class="icon-plus"></span>
+            Tambah
+          </button>
+        </div>
+      </div>
+      <div class="mt-8">
+        <div
+          data-cy="todo-item-list"
+          v-if="activityDetailData.todo_items?.length !== 0"
+          class="flex flex-col flex-wrap gap-4 justify-center"
+        >
+          <todo-item-card
+            v-for="todoItem in activityDetailData.todo_items"
+            data-cy="todo-item"
+            :key="todoItem.id"
+            :todoItem="todoItem"
+            @get-todo-detail="detailTodoItem"
+          />
+        </div>
+        <empty-state-image v-else pageName="activity-detail" />
+      </div>
+    </div>
+    <input-dialog
+      data-cy="todo-item-input-dialog"
+      :inputMode="inputMode"
+      :selectedTodo="selectedTodo"
+    />
+    <confirm-dialog data-cy="modal-delete" />
+  </div>
+</template>
+
+<script>
+import EmptyStateImage from "@/components/EmptyStateImage.vue";
+import TodoItemCard from "@/components/TodoItemCard.vue";
+import InputDialog from "@/components/InputDialog.vue";
+import ConfirmDialog from "@/components/ConfirmDialog.vue";
+
+import store from "@/store";
+import { mapGetters } from "vuex";
+
+export default {
+  name: "ActivityDetailView",
+
+  components: {
+    TodoItemCard,
+    EmptyStateImage,
+    InputDialog,
+    ConfirmDialog,
+  },
+
+  data() {
+    return {
+      selectedTodo: {},
+      activityDetailData: {},
+      showInputText: false,
+      inputTextValue: "",
+      showDialog: false,
+    };
+  },
+
+  computed: mapGetters({
+    inputMode: "inputMode",
+  }),
+
+  mounted() {
+    this.loadDetailActivity();
+  },
+
+  methods: {
+    toggleInputDialog(value) {
+      store.dispatch("setInputMode", {
+        data: "add",
+      });
+      store.dispatch("toggleInputDialog", { value });
+    },
+    resetSelectedTodo() {
+      this.selectedTodo = {};
+    },
+    async loadDetailActivity() {
+      try {
+        this.activityDetailData = await store.dispatch("detailActivity", {
+          id: this.$route.params.id,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async detailTodoItem(id) {
+      try {
+        this.selectedTodo = await store.dispatch("detailTodo", {
+          id,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async changeActivityTitle() {
+      if (this.showInputText) {
+        try {
+          await store.dispatch("updateActivity", {
+            id: this.$route.params.id,
+            title: this.inputTextValue,
+          });
+          this.loadDetailActivity();
+          this.showInputText = false;
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+        this.inputTextValue = this.activityDetailData?.title;
+        this.showInputText = true;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/icons/icons";
+</style>
